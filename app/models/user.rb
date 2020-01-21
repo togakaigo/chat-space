ba069b4a5fed50972be992fd0414fc48cc4fb54f
togakaigo <59080958+togakaigo@users.noreply.github.com>
2020-01-21 20:37:11 +0900

    修正しました。

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  has_many :members
+  has_many :groups, through: :members
+  has_many :messages
+end
